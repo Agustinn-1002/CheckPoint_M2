1f23bcac3288fd88b203ff0a6c93ccb99c6b4bb4
juanmarsilva <juanmar_silva@hotmail.com>
2022-05-28 10:28:28 -0300

    Test 06 pasado

--- src/components/CreateProduct/CreateProduct.jsx
+++ src/components/CreateProduct/CreateProduct.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { createProduct } from '../../redux/actions';
 
 // Fijense en los test que SI O SI tiene que ser un functional component, de otra forma NO VAN A PASAR LOS TEST
 // Deben usar Hooks para que los test pasen.
@@ -6,8 +8,44 @@ import React from 'react';
 // Si no lo hacen asi los test no van a correr.
 
 const CreateProduct = () => {
+
+   const [input, setInput] = React.useState({
+      name: '',
+      price: 0,
+      description: '',
+      stock: 0,
+   })
+
+   const handleInputChange = (event) => {
+      setInput({
+         ...input,
+         [event.target.name]: event.target.value
+      })
+   }
+
+
+   const dispatch = useDispatch();
+   const handleSubmit = (event) => {
+      event.preventDefault();
+      dispatch(createProduct(input));
+   }
+
   return (
-      <div>Create Product</div>
+      <form onSubmit={handleSubmit}>
+         <label>Name: </label>
+         <input name='name' onChange={handleInputChange}></input>
+
+         <label>Price: </label>
+         <input type='number' name='price' onChange={handleInputChange}></input>
+
+         <label>Description: </label>
+         <textarea name='description' onChange={handleInputChange}></textarea>
+
+         <label>Stock: </label>
+         <input type='number' name='stock' onChange={handleInputChange}></input>
+         
+         <button type='submit'>Create Product</button>
+      </form>
    );
 };
 
